7446b6b4d300ea68c8e783b58a6ce40fe428bc41
danialzadrafiee <leylasarafrezai@gmail.com>
2024-03-22 00:00:17 +0330

    update

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,8 @@ use App\Http\Controllers\GatewayController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::auto("/",GatewayController::class);
-Route::auto("/dashboard",DashboardController::class);
+Route::auto("/", GatewayController::class);
+Route::auto("/dashboard", DashboardController::class);
+Route::get('/webhook', function () {
+    shell_exec('/bin/bash /var/www/pepoleon.developerpie.com/public/pull.sh');
+});
