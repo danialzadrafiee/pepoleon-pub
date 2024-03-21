d0ebe467426cad029cf9d7951b2aff8ce01926ec
danialzadrafiee <leylasarafrezai@gmail.com>
2024-03-21 23:34:50 +0330

    update

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,7 +8,7 @@
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=Poppins:400,500,600&display=swap" rel="stylesheet" />
+        <link href="https://fonts.bunny.net/css?family=Poppins:400,500,600,700,800,900&display=swap" rel="stylesheet" />
         <link href="https://fonts.cdnfonts.com/css/impact" rel="stylesheet">
         <!-- Scripts -->
         @routes
