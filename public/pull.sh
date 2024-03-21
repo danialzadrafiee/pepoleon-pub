201372365a300229593c9aa2e2cfbdb55593bc11
danialzadrafiee <leylasarafrezai@gmail.com>
2024-03-22 00:10:45 +0330

    update

--- public/pull.sh
+++ public/pull.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /var/www/pepoleon.developerpie.com || exit
 unset GIT_DIR
-git pull origin main
+git pull origin main 2>&1 
--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,12 @@ Route::auto("/", GatewayController::class);
 Route::auto("/dashboard", DashboardController::class);
 Route::get('/webhook', function () {
     $output = shell_exec('/bin/bash /var/www/pepoleon.developerpie.com/public/pull.sh');
-    return response($output, 200)
-                  ->header('Content-Type', 'text/plain');
+
+    if (strpos($output, 'error') !== false) {
+        return response($output, 500) // Indicate error with status code 500
+            ->header('Content-Type', 'text/plain');
+    } else {
+        return response($output, 200)
+            ->header('Content-Type', 'text/plain');
+    }
 });
